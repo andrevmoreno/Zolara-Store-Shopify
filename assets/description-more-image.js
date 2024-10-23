b326e512221a708957d54d01f530482b6d1b7bc5
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-10-22 22:43:55 -0300

    Update description-more-image.js

--- assets/description-more-image.js
+++ assets/description-more-image.js
@@ -1,22 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const tabTitles = document.querySelectorAll('.tab-title')
+  const tabs = document.querySelectorAll('.tab')
   const tabContents = document.querySelectorAll('.tab-content')
 
-  tabTitles.forEach((tab, index) => {
-    tab.addEventListener('click', function () {
-      // Remover la clase "active" de todos los tabs y contenidos
-      tabTitles.forEach((t) => t.classList.remove('active'))
-      tabContents.forEach((c) => c.classList.remove('active'))
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((t) => t.classList.remove('active'))
+      tabContents.forEach((tc) => tc.classList.remove('active'))
 
-      // Agregar la clase "active" al tab y contenido clicado
       tab.classList.add('active')
-      tabContents[index].classList.add('active')
+      document.getElementById(tab.dataset.tab).classList.add('active')
     })
   })
-
-  // Activar la primera pestaña por defecto
-  if (tabTitles.length > 0) {
-    tabTitles[0].classList.add('active')
-    tabContents[0].classList.add('active')
-  }
 })
